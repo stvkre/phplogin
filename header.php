7bde6943c734a6afd52ccee6db9a49842cf7a4f7
stvkre <kiariemburuh@gmail.com>
2017-11-16 13:23:28 +0300

    Php intergration
    -Added php together with css to the homepage

---
+++ header.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="header.css">
+<?php
+    echo '<a href="/default.asp">HOME</a>
+    <a href="/html/default.asp">PRODUCTS</a>
+    <a href="/css/default.asp">ABOUT</a>
+    <a href="/js/default.asp">CONTACTS</a>';
+?>
